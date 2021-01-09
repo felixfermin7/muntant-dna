67070f9cd917e99f7092ad92e09455f725d77081
felixfermin7 <felixfermin7@gmail.com>
2021-01-09 14:12:48 -0500

    Register muntant data

--- migrations/20210108192835_create_mutants_table.js
+++ migrations/20210108192835_create_mutants_table.js
@@ -2,7 +2,8 @@ const { MUTANTS } = require('../lib/src/repositories/tableNames')
 
 module.exports.up = knex =>
   knex.schema.createTable(MUTANTS, table => {
-    table.string('dna').primary()
+    table.string('id').primary()
+    table.specificType('dna', 'varchar[]').notNullable()
     table.bool('is_mutant').notNullable()
     table.timestamps(true, true)
   })
